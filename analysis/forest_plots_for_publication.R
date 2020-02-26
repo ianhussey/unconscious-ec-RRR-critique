133ef458699c181bfbbe3bf6aafe73d6374d4de0
ianhussey <ian.hussey@ugent.be>
2020-02-26 19:49:41 +0100

    improved forest plots

--- analysis/forest_plots_for_publication.R
+++ analysis/forest_plots_for_publication.R
@@ -6,7 +6,23 @@ setwd("~/git/unconscious-ec-RRR-critique/analysis")
 pdf(NULL)
 dev.control(displaylist = "enable")
 
+par(mfrow = c(2, 1))
+
 size <- 1
+
+metafor::forest(fit_moderator, 
+                ilab = round(data_moderator_meta$exclude_aware_olsen_and_fazio, 0),
+                ilab.xpos = 1.15,
+                ilab.pos = 2,
+                xlab = substitute(paste("Hedges' ", italic('g'))),
+                addcred = TRUE,
+                xlim = c(-1.4, 1.8),
+                at = c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0),
+                refline = 0)
+text(-1.4, 13.95, "Olson & Fazio (2001) exclusions controlling for awareness rate", pos = 4)
+text(1.2, 13.95, "% Aware", pos = 2)
+text(1.8, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
+
 
 metafor::forest(results_combined_criteria$fitted_model,
                 xlab = substitute(paste("Hedges' ", italic('g'))),
@@ -15,8 +31,9 @@ metafor::forest(results_combined_criteria$fitted_model,
                 at = c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2),
                 refline = 0,
                 mlab = add_heterogeneity_metrics_to_forest(results_combined_criteria$fitted_model))
-text(-1.4, 14, "Combined exclusion criteria", pos = 4)
+text(-1.4, 14, "Compound exclusion criterion", pos = 4)
 text(1.8, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
+
 
 p1 <- recordPlot()
 invisible(dev.off())
@@ -25,8 +42,8 @@ invisible(dev.off())
 # grid::grid.newpage()
 # p1
 
-pdf("forest_plot.pdf",
+pdf("forest_plots.pdf",
     width = 10, 
-    height = 6)
+    height = 12)
 p1
 dev.off()
