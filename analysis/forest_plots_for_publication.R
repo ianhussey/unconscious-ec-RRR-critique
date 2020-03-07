d47ab461cfd48d101dd3d5ad72eff1a209ed2799
ianhussey <ian.hussey@ugent.be>
2020-03-07 15:52:57 +0100

    manuscript and analysis updates

--- analysis/forest_plots_for_publication.R
+++ analysis/forest_plots_for_publication.R
@@ -10,20 +10,6 @@ par(mfrow = c(2, 1))
 
 size <- 1
 
-metafor::forest(fit_moderator, 
-                ilab = round(data_moderator_meta$exclude_aware_olsen_and_fazio, 0),
-                ilab.xpos = 1.15,
-                ilab.pos = 2,
-                xlab = substitute(paste("Hedges' ", italic('g'))),
-                addcred = TRUE,
-                xlim = c(-1.4, 1.8),
-                at = c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0),
-                refline = 0)
-text(-1.4, 13.95, "Olson & Fazio (2001) exclusions controlling for awareness rate", pos = 4)
-text(1.2, 13.95, "% Aware", pos = 2)
-text(1.8, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
-
-
 metafor::forest(results_combined_criteria$fitted_model,
                 xlab = substitute(paste("Hedges' ", italic('g'))),
                 addcred = TRUE,
@@ -31,9 +17,22 @@ metafor::forest(results_combined_criteria$fitted_model,
                 at = c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2),
                 refline = 0,
                 mlab = add_heterogeneity_metrics_to_forest(results_combined_criteria$fitted_model))
-text(-1.4, 14, "Compound exclusion criterion", pos = 4)
+text(-1.4, 14, "Compound awareness exclusion criterion", pos = 4)
 text(1.8, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
 
+
+metafor::forest(fit_moderator, 
+                ilab = round(data_moderator_meta$exclude_all_four_combined, 2)*100,
+                ilab.xpos = 1.2,
+                ilab.pos = 2,
+                xlab = substitute(paste("Hedges' ", italic('g'))),
+                addcred = TRUE,
+                xlim = c(-1.4, 1.8),
+                at = c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0),
+                refline = 0)
+text(-1.4, 13.95, "Moderation by site exclusion rate", pos = 4)
+text(1.2, 13.95, "% Aware", pos = 2)
+text(1.8, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
 
 p1 <- recordPlot()
 invisible(dev.off())
