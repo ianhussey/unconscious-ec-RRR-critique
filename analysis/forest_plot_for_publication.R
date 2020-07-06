af89f442d7d99cf49a0da3cbeba58daa6a8aee06
ianhussey <ian.hussey@ugent.be>
2020-07-06 17:02:49 +0200

    updated forest plot and preprint

--- analysis/forest_plot_for_publication.R
+++ analysis/forest_plot_for_publication.R
@@ -1,7 +1,19 @@
 # Open and run analysis.Rmd so that the results_combined_criteria object is present in your environment. 
 # Then run this script. 
 
+library(tidyverse)
+library(metafor)
+
+# add heterogeneity metrics to metafor forest plot
+add_heterogeneity_metrics_to_forest <- function(fit) {
+  bquote(paste("RE Model (", 
+               italic('I')^"2", " = ", .(formatC(format(round(fit$I2, 1), nsmall = 1))),
+               "%, ", italic('H')^"2", " = ", .(formatC(format(round(fit$H2, 1), nsmall = 1))), ")"))
+}
+
 setwd("~/git/unconscious-ec-RRR-critique/analysis")
+results_combined_criteria <- read_rds("models/results_combined_criteria.rds")
+
 
 pdf(NULL)
 dev.control(displaylist = "enable")
@@ -13,12 +25,12 @@ size <- 1
 metafor::forest(results_combined_criteria$fitted_model,
                 xlab = substitute(paste("Hedges' ", italic('g'))),
                 addcred = TRUE,
-                xlim = c(-1.4, 1.8),
+                xlim = c(-1.6, 2.1),
                 at = c(-0.8, -0.4, 0, 0.4, 0.8, 1.2),
                 refline = 0,
                 mlab = add_heterogeneity_metrics_to_forest(results_combined_criteria$fitted_model))
-text(-1.4, 14, "Site", pos = 4)
-text(1.8, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
+text(-1.6, 14, "Site", pos = 4)
+text(2.1, 13.85, substitute(paste("Hedges' ", italic('g'), " [95% CI]")), pos = 2)
 
 p1 <- recordPlot()
 invisible(dev.off())
